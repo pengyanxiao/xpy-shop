93f8a61db759f28b30a6d69f964c38f1d428498d
xiaopengyan36 <xiaopengyan36@163.com>
2020-08-27 19:16:36 +0800

    aa

--- mr-shop-parent/mr-shop-basics/mr-shop-basic-eureka-server/src/main/java/com/baidu/TestEurekaServerApplication.java
+++ mr-shop-parent/mr-shop-basics/mr-shop-basic-eureka-server/src/main/java/com/baidu/TestEurekaServerApplication.java
@@ -16,7 +16,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class TestEurekaServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("aaa");
         SpringApplication.run(TestEurekaServerApplication.class);
     }
 
