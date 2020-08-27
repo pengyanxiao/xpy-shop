095605ca2dca0aadb8c5a5da8f4efa87fdbc412b
xiaopengyan36 <xiaopengyan36@163.com>
2020-08-27 15:24:46 +0800

    first commit

---
+++ mr-shop-parent/mr-shop-basics/mr-shop-basic-eureka-server/src/main/java/com/baidu/TestEurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName TestEurekaServerApplication
+ * @Description: TODO
+ * @Author xiaopengyan
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class TestEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestEurekaServerApplication.class);
+    }
+
+}
